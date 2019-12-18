c31f26b50d9fa6d97437f87dcda2de0a6191aca1
Nhu Quynh <you@example.com>
2019-12-18 15:43:37 +0700

    review_book(tt)

---
+++ Homepage/book_online.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.body.classList.add("loaded");
+});
+window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}gtag('js',new Date());gtag('config','UA-129266625-1');
+var OneSignal=window.OneSignal||[];OneSignal.push(function(){OneSignal.init({appId:"540c08d0-b3f7-4e0a-92da-402c12adc14d",});});
